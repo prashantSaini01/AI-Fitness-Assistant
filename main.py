276bc2fbf44f7e135b557e5b25b0d3eb372bbd7f
Prashant Saini <sainiprashant300@gmail.com>
2024-11-26 12:24:47 +0530

    bug fixed #1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fitness_assistant import create_interface
+import os
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     demo = create_interface()
-    demo.launch(share=True)
+    #demo.launch(share=True)
+    demo.launch(server_name="0.0.0.0", server_port=port)
